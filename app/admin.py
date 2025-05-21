3c1581d1ef4749b7ef0b593bc94c7b3ff063acf8
Rolo Wilde <git@rolowil.de>
2025-05-21 09:34:27 +0300

    Add manufacturers to admin

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import *
 
-models = [Socket, Processor, ProcessorCooler, GraphicsCard, CaseFormFactor, Case, CaseFan, Motherboard, Memory,
-          StorageFormFactor, StorageInterface, Storage, PowerSupply, Build]
+models = [Manufacturer, Socket, Processor, ProcessorCooler, GraphicsCard, CaseFormFactor, Case, CaseFan, Motherboard,
+          Memory, StorageFormFactor, StorageInterface, Storage, PowerSupply, Build]
 
 for model in models:
     admin.site.register(model)
